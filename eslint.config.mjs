3f390aa023d44b2b0d85f66a326057234cd02dd9
Kadhir Manickam <kadhirvelm@gmail.com>
2024-07-28 21:53:08 -0700

    Add API definitions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,22 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import typescriptSort from "eslint-plugin-typescript-sort-keys";
+import objectSort from "eslint-plugin-sort-keys-fix";
 
 export default [
   ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict),
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      "sort-keys-fix": objectSort,
+      "typescript-sort-keys": typescriptSort
+    },
     rules: {
-      "@typescript-eslint/no-extraneous-class": 0
+      "@typescript-eslint/no-extraneous-class": 0,
+      "sort-keys-fix/sort-keys-fix": "error",
+      "typescript-sort-keys/interface": "error",
+      "typescript-sort-keys/string-enum": "error"
     }
   }
 ];
