6b9bbf2da81a2107107d7fd90fcad028597c0bd0
Kadhir Manickam <kadhirvelm@gmail.com>
2024-08-03 09:34:55 -0700

    Update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import typescriptSort from "eslint-plugin-typescript-sort-keys";
 import objectSort from "eslint-plugin-sort-keys-fix";
+import nextEslintPlugin from "@next/eslint-plugin-next";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
   ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict),
@@ -19,6 +21,18 @@ export default [
       "sort-keys-fix/sort-keys-fix": "error",
       "typescript-sort-keys/interface": "error",
       "typescript-sort-keys/string-enum": "error"
+    }
+  },
+  {
+    files: ["**/frontend/**/*.{ts,tsx}"],
+    plugins: {
+      "@next/next": nextEslintPlugin,
+      "react-hooks": reactHooksPlugin
+    },
+    rules: {
+      ...nextEslintPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "@next/next/no-duplicate-head": 0
     }
   },
   {
