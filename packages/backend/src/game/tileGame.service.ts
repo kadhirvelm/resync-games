66b1f6fae82ce89f03ef75e76d578c0db1632b91
Kadhir <kadhirvelm@gmail.com>
2024-08-10 18:56:02 -0700

    Add backend implementation for games (#22)

---
+++ packages/backend/src/game/tileGame.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import _ from "lodash";
+import { InitializeTileGameRequest, TileGameId } from "@tiles-tbd/api";
+import { TileMapService } from "src/map/tileMap.service";
+import { TILE_GAME_PAWN_COLORS } from "./tileGame.constants";
+
+@Injectable()
+export class TileGameService {
+  public constructor(
+    private prismaService: PrismaService,
+    private tileMapService: TileMapService
+  ) {}
+
+  public getAvailableGames = async () => {
+    const allGames = await this.prismaService.client.tileGame.findMany({
+      include: { tileMap: true }
+    });
+    const allMaps = _.uniqBy(
+      allGames.flatMap((game) => game.tileMap),
+      (map) => map.tileMapId
+    );
+
+    return {
+      tileGames: allGames.map(
+        this.prismaService.converterService.convertTileGame
+      ),
+      tileMaps: allMaps.map(this.prismaService.converterService.convertMap)
+    };
+  };
+
+  public getTileGame = async (gameId: TileGameId) => {
+    const game = await this.prismaService.client.tileGame.findUnique({
+      include: { TilePawn: true },
+      where: { tileGameId: gameId }
+    });
+
+    const tileGame = this.prismaService.converterService.convertTileGame(game);
+    const pawns = game.TilePawn.flatMap((pawn) =>
+      this.prismaService.converterService.convertTilePawn(pawn)
+    );
+
+    const tileMap = await this.tileMapService.getTileMap(tileGame.tileMapId);
+
+    return {
+      game: {
+        ...tileGame,
+        pawns
+      },
+      tileMap
+    };
+  };
+
+  public initializeGame = async (
+    initializeGameRequest: InitializeTileGameRequest
+  ) => {
+    const tileMap = await this.tileMapService.getTileMap(
+      initializeGameRequest.tileMapId
+    );
+
+    const pawns = _.range(initializeGameRequest.numberOfPawns).map((index) => ({
+      color: TILE_GAME_PAWN_COLORS[index],
+      onTileId: tileMap.tileMap.startingTileId
+    }));
+
+    const newGame = await this.prismaService.client.tileGame.create({
+      data: {
+        TilePawn: {
+          create: pawns
+        },
+        name: initializeGameRequest.name,
+        state: "waiting",
+        tileMapId: initializeGameRequest.tileMapId
+      }
+    });
+
+    return {
+      game: this.prismaService.converterService.convertTileGame(newGame)
+    };
+  };
+}
--- packages/backend/src/map/tileMap.module.ts
+++ packages/backend/src/map/tileMap.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "src/database/prisma.service";
+import { PrismaModule } from "src/database/prisma.module";
 import { TileMapController } from "./tileMap.controller";
 import { TileMapService } from "./tileMap.service";
-import { ConverterService } from "src/database/converter.service";
 
 @Module({
   controllers: [TileMapController],
   exports: [TileMapService],
-  providers: [TileMapService, PrismaService, ConverterService]
+  imports: [PrismaModule],
+  providers: [TileMapService]
 })
 export class TileMapModule {}
