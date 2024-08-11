3ca725587a8a5de79a384641e463be5698292f19
Kadhir <kadhirvelm@gmail.com>
2024-08-10 18:33:35 -0700

    Ad basic game API definition (#20)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,9 +2,11 @@ export * from "./genericTypes/error";
 export * from "./genericTypes/removeExtendsString";
 export * from "./genericTypes/service";
 
-export * from "./services/map/types";
+export * from "./services/tileGame/types";
+export * from "./services/tileMap/types";
 
 export * from "./services/health";
+export * from "./services/tileGame";
 export * from "./services/tileMap";
 
 export * from "./availableServices";
---
+++ packages/api/src/services/tileGame/types.ts
@@ -0,0 +1,21 @@
+import { TileId, TileMapId } from "../tileMap/types";
+
+export type TileGameId = string & { __brand: "tile-game-id" };
+
+export type PawnId = string & { __brand: "pawn-id" };
+
+export interface TilePawn {
+  color: string;
+  onTile: TileId;
+  pawnId: PawnId;
+}
+
+export type TileGameState = "waiting" | "playing" | "finished";
+
+export interface TileGame {
+  name: string;
+  pawns: TilePawn[];
+  state: TileGameState;
+  tileGameId: TileGameId;
+  tileMapId: TileMapId;
+}
