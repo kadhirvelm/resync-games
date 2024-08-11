cbbd8a2891d22bea92b088584500a8e2a918d393
Kadhir <kadhirvelm@gmail.com>
2024-08-11 12:19:55 -0700

    Display the current position of the pawns (#27)

--- packages/frontend/src/components/TileMap.tsx
+++ packages/frontend/src/components/TileMap.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { CompleteTileMap as ICompleteTileMap } from "@tiles-tbd/api";
+import { CompleteTileMap as ICompleteTileMap, TilePawn } from "@tiles-tbd/api";
 import { DisplayTile } from "./DisplayTile";
 import { indexTileMap } from "./utils/indexTileMap";
 import { Box } from "grommet";
+import { indexPawns } from "./utils/indexPawns";
 
-export const TileMap = ({ tileMap }: { tileMap: ICompleteTileMap }) => {
+export const TileMap = ({
+  tileMap,
+  pawns
+}: {
+  pawns?: TilePawn[];
+  tileMap: ICompleteTileMap;
+}) => {
   const { outboundEdges, tilesIndexed } = indexTileMap(tileMap);
+  const { indexedPawns } = indexPawns(pawns ?? []);
+
+  console.log(indexedPawns);
 
   return (
     <Box direction="row" style={{ padding: "10px" }}>
       <DisplayTile
         tilesIndexed={tilesIndexed}
         outboundEdgesIndexed={outboundEdges}
+        indexedPawns={indexedPawns}
         tileId={tileMap.tileMap.startingTileId}
       />
     </Box>
