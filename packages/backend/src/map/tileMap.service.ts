04a4dce87628665e17704bc9330504e7ac4d93bb
Kadhir <kadhirvelm@gmail.com>
2024-08-11 00:56:09 -0700

    Clean up the prisma service (#23)

--- packages/backend/src/map/tileMap.service.ts
+++ packages/backend/src/map/tileMap.service.ts
@@ -9,10 +9,26 @@ export class TileMapService {
   public getTileMap = async (
     tileMapId: TileMapId
   ): Promise<CompleteTileMap> => {
-    return this.prismaService.getTileMap(tileMapId);
+    const tileMap = await this.prismaService.client.tileMap.findFirst({
+      include: { tiles: { include: { fromTile: true } } },
+      where: { tileMapId }
+    });
+
+    const edges = tileMap.tiles.flatMap((tile) => tile.fromTile);
+
+    return {
+      edges: edges.map(this.prismaService.converterService.convertEdge),
+      tileMap: this.prismaService.converterService.convertMap({
+        createdAt: tileMap.createdAt,
+        startingTileId: tileMap.startingTileId,
+        tileMapId: tileMap.tileMapId
+      }),
+      tiles: tileMap.tiles.map(this.prismaService.converterService.convertTile)
+    };
   };
 
   public getAllTileMaps = async (): Promise<TileMap[]> => {
-    return this.prismaService.getAllTileMaps();
+    const allMaps = await this.prismaService.client.tileMap.findMany({});
+    return allMaps.map(this.prismaService.converterService.convertMap);
   };
 }
