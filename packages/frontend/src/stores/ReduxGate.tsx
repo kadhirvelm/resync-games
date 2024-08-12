b14f27adb90a34897b4386c937e472ac7f483abc
Kadhir Manickam <kadhirvelm@gmail.com>
2024-08-11 19:35:07 -0700

    Add super basic pawn movements

---
+++ packages/frontend/src/stores/ReduxGate.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, EnhancedStore, UnknownAction } from "@reduxjs/toolkit";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+
+export function ReduxGate<T extends EnhancedStore>({
+  createStore,
+  children,
+  initializeStore
+}: {
+  children: React.ReactNode;
+  createStore: () => T;
+  initializeStore: (dispatch: Dispatch<UnknownAction>) => void;
+}) {
+  const storeRef = useRef<T>();
+  if (!storeRef.current) {
+    storeRef.current = createStore();
+    initializeStore(storeRef.current.dispatch);
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
