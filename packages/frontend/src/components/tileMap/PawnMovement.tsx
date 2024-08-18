e1baa152d53bedc3ae0de49582242f1739622deb
Kadhir <kadhirvelm@gmail.com>
2024-08-17 21:33:24 -0700

    Allow moving pawns with arrows (#32)
    <img width="242" alt="Screenshot 2024-08-17 at 9 29 29 PM" src="https://github.com/user-attachments/assets/0bed54e1-1d97-4342-9e41-92f9487f490c">

--- packages/frontend/src/components/tileMap/PawnMovement.tsx
+++ packages/frontend/src/components/tileMap/PawnMovement.tsx
@@ -1,34 +1,49 @@
-import { useTileDispatch, useTileSelector } from "@/stores/tiles/tilesStore";
-import { isServiceError, TilePawn } from "@tiles-tbd/api";
-import { Box } from "grommet";
-import styles from "./PawnMovement.module.scss";
 import { ClientServiceCallers } from "@/services/serviceCallers";
 import { updatePawn } from "@/stores/tiles/pawnState";
-import { shuffle } from "lodash-es";
+import { useTileDispatch, useTileSelector } from "@/stores/tiles/tilesStore";
+import { Edge, isServiceError } from "@tiles-tbd/api";
+import { Box, Button } from "grommet";
+import {
+  CaretDownFill,
+  CaretLeftFill,
+  CaretRightFill,
+  CaretUpFill
+} from "grommet-icons";
+import styles from "./PawnMovement.module.scss";
+
+const flavorTextToIcon = {
+  DOWN: <CaretDownFill size="75px" />,
+  LEFT: <CaretLeftFill size="75px" />,
+  RIGHT: <CaretRightFill size="75px" />,
+  UP: <CaretUpFill size="75px" />
+};
+
+const flavorTextToPosition = {
+  DOWN: { bottom: 0, left: "50%", transform: "translateX(-50%)" },
+  LEFT: { left: 0, top: "50%", transform: "translateY(-50%)" },
+  RIGHT: { right: 0, top: "50%", transform: "translateY(-50%)" },
+  UP: { left: "50%", top: 0, transform: "translateX(-50%)" }
+};
 
 export function PawnMovement() {
   const dispatch = useTileDispatch();
-
-  const { pawnState, outboundEdges } = useTileSelector(
+  const { pawnState, outboundEdges, selectedPawnId } = useTileSelector(
     (state) => state.pawnState
   );
 
-  const onTemporaryMovePawn = (pawn: TilePawn) => async () => {
-    let outboundEdgesFromTile = outboundEdges[pawn.onTileId];
-    // Shuffle the edges to simulate randomness
-    if (outboundEdgesFromTile) {
-      outboundEdgesFromTile = shuffle(outboundEdgesFromTile);
-    }
+  const selectedPawn =
+    selectedPawnId !== undefined ? pawnState[selectedPawnId] : undefined;
+  if (selectedPawn === undefined) {
+    return;
+  }
 
-    const nextTile = outboundEdgesFromTile?.[0]?.toTileId;
-    if (nextTile === undefined) {
-      return;
-    }
+  const outboundEdgesForPawn = outboundEdges[selectedPawn.onTileId] ?? [];
 
+  const onMovePawn = (edge: Edge) => async () => {
     const updatedPawn = await ClientServiceCallers.tileGame.movePawn({
-      fromTileId: pawn.onTileId,
-      tilePawnId: pawn.tilePawnId,
-      toTileId: nextTile
+      fromTileId: selectedPawn.onTileId,
+      tilePawnId: selectedPawn.tilePawnId,
+      toTileId: edge.toTileId
     });
     if (isServiceError(updatedPawn)) {
       return;
@@ -38,12 +53,25 @@ export function PawnMovement() {
   };
 
   return (
-    <Box className={styles.pawnMovementContainer} gap="10px">
-      {Object.entries(pawnState).map(([pawnId, pawn]: [string, TilePawn]) => (
-        <Box key={pawnId} onClick={onTemporaryMovePawn(pawn)}>
-          {pawn.color} - {pawn.onTileId.slice(-4)}
-        </Box>
+    <Box className={styles.pawnMovementContainer} flex="grow">
+      {outboundEdgesForPawn.map((edge) => (
+        <Button
+          className={styles.movement}
+          key={edge.edgeId}
+          onClick={onMovePawn(edge)}
+          style={
+            flavorTextToPosition[
+              edge.flavorText as keyof typeof flavorTextToPosition
+            ]
+          }
+        >
+          {flavorTextToIcon[edge.flavorText as keyof typeof flavorTextToIcon]}
+        </Button>
       ))}
+      <Box
+        className={styles.colorIndicator}
+        style={{ background: selectedPawn.color }}
+      />
     </Box>
   );
 }
