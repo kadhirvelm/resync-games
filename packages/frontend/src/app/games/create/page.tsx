c6dcc932d46e68930c736ef55d785eebbec32bc4
Rohan Bavishi <rohan.bavishi95@gmail.com>
2024-10-03 00:51:52 -0400

    Fixes

--- packages/frontend/src/app/games/create/page.tsx
+++ packages/frontend/src/app/games/create/page.tsx
@@ -1,20 +1,33 @@
 "use client";
 
 import { NavigationButton } from "@/lib/tiles-components/NavigationButton";
-import { GAME_REGISTRY } from "@resync-games/games";
+import dynamic from "next/dynamic";
 
 export default function NavigateToGameCreate() {
+  // Lazy load the game component only on the client-side
+  const DynamicComponent = dynamic(
+    () =>
+      import("@resync-games/games").then((module) => {
+        const { GAME_REGISTRY } = module;
+        return () => (
+          <div>
+            {Object.entries(GAME_REGISTRY).map(([key, { slug, name }]) => (
+              <div>
+                <NavigationButton href={`/${slug}/create`} key={key}>
+                  {name}
+                </NavigationButton>
+                <br></br>
+              </div>
+            ))}
+          </div>
+        );
+      }),
+    { ssr: false } // Disable server-side rendering
+  );
   return (
     <div>
       <h1>Pick game to create</h1>
-      {Object.entries(GAME_REGISTRY).map(([key, { slug, name }]) => (
-        <div>
-          <NavigationButton href={`/${slug}/create`} key={key}>
-            {name}
-          </NavigationButton>
-          <br></br>
-        </div>
-      ))}
+      <DynamicComponent />
     </div>
   );
 }
