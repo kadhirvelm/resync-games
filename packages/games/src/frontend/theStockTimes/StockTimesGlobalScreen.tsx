c762fb63c51590858c409fcd8a381a33cacc922e
Kadhir Manickam <kadhirvelm@gmail.com>
2024-12-29 15:50:00 -0800

    Implement global screen for stock times

--- packages/games/src/frontend/theStockTimes/StockTimesGlobalScreen.tsx
+++ packages/games/src/frontend/theStockTimes/StockTimesGlobalScreen.tsx
@@ -1,14 +1,26 @@
 import { Flex } from "../components";
+import { Clock } from "./components/cycle/Clock";
+import { FinalScoreboard } from "./components/FinalScoreboard";
 import { useGameStateSelector } from "./store/theStockTimesRedux";
 
 export const StockTimesGlobalScreen = () => {
   const gameInfo = useGameStateSelector((s) => s.gameStateSlice.gameInfo);
   const gameState = useGameStateSelector((s) => s.gameStateSlice.gameState);
 
+  if (gameState === undefined) {
+    return;
+  }
+
   return (
-    <Flex>
-      {JSON.stringify(gameInfo, null, 2)}
-      {JSON.stringify(gameState, null, 2)}
+    <Flex gap="2">
+      <Flex flex="1">
+        <FinalScoreboard />
+      </Flex>
+      {gameInfo?.currentGameState === "playing" && (
+        <Flex flex="1">
+          <Clock cycle={gameState?.cycle} size={window.innerWidth / 2} />
+        </Flex>
+      )}
     </Flex>
   );
 };
--- packages/games/src/frontend/theStockTimes/components/cycle/Clock.tsx
+++ packages/games/src/frontend/theStockTimes/components/cycle/Clock.tsx
@@ -4,63 +4,84 @@ import { Flex } from "../../../components";
 import { useCycleTime } from "../../hooks/cycleTime";
 import styles from "./Clock.module.scss";
 
-const size = 75;
+const DEFAULT_SIZE = 75;
 
-const radius = size / 2 - 1;
-const centerX = size / 2;
-const centerY = size / 2;
+const radius = (size: number) => size / 2 - 1;
+const centerX = (size: number) => size / 2;
+const centerY = (size: number) => size / 2;
 
-function calculateNightTimeArc(cycle: StockTimesCycle) {
+function calculateNightTimeArc(cycle: StockTimesCycle, size: number) {
   const percentNight = cycle.nightTime / (cycle.dayTime + cycle.nightTime);
   const angle = percentNight * 360;
 
-  const endX = centerX + radius * Math.cos((angle - 90) * (Math.PI / 180));
-  const endY = centerY + radius * Math.sin((angle - 90) * (Math.PI / 180));
+  const endX =
+    centerX(size) + radius(size) * Math.cos((angle - 90) * (Math.PI / 180));
+  const endY =
+    centerY(size) + radius(size) * Math.sin((angle - 90) * (Math.PI / 180));
 
   const largeArcFlag = angle > 180 ? 1 : 0;
 
-  return `M ${centerX} ${centerY}
-             L ${centerX} ${centerY - radius}
-             A ${radius} ${radius} 0 ${largeArcFlag} 1 ${endX} ${endY}
+  return `M ${centerX(size)} ${centerY(size)}
+             L ${centerX(size)} ${centerY(size) - radius(size)}
+             A ${radius(size)} ${radius(size)} 0 ${largeArcFlag} 1 ${endX} ${endY}
              Z`;
 }
 
-function calculateClockPointer(timeFraction: number) {
+function calculateClockPointer(timeFraction: number, size: number) {
   const angle = timeFraction * 360;
 
   const endX =
-    centerX + radius * Math.cos((angle - 90) * (Math.PI / 180)) * 0.95;
+    centerX(size) +
+    radius(size) * Math.cos((angle - 90) * (Math.PI / 180)) * 0.95;
   const endY =
-    centerY + radius * Math.sin((angle - 90) * (Math.PI / 180)) * 0.95;
+    centerY(size) +
+    radius(size) * Math.sin((angle - 90) * (Math.PI / 180)) * 0.95;
 
-  return `M ${centerX} ${centerY} L ${endX} ${endY}`;
+  return `M ${centerX(size)} ${centerY(size)} L ${endX} ${endY}`;
 }
 
-export const Clock = ({ cycle }: { cycle: StockTimesCycle }) => {
+export const Clock = ({
+  cycle,
+  size
+}: {
+  cycle: StockTimesCycle;
+  size?: number;
+}) => {
   const calculatedCycleTime = useCycleTime(cycle);
+  const finalSize = size ?? DEFAULT_SIZE;
 
   const { day, timeFraction } = calculatedCycleTime;
 
   return (
     <>
+      <Flex
+        className={clsx(styles.background, {
+          [styles.day ?? ""]: calculatedCycleTime.currentCycle === "day",
+          [styles.night ?? ""]: calculatedCycleTime.currentCycle === "night"
+        })}
+      />
       <Flex align="center" gap="1">
         <svg height={size} width={size}>
           <circle
             className={styles.baseClock}
-            cx={centerX}
-            cy={centerY}
-            r={radius}
+            cx={centerX(finalSize)}
+            cy={centerY(finalSize)}
+            r={radius(finalSize)}
           />
-          <path className={styles.night} d={calculateNightTimeArc(cycle)} />
+          <path
+            className={styles.night}
+            d={calculateNightTimeArc(cycle, finalSize)}
+          />
           <path
             className={styles.pointer}
-            d={calculateClockPointer(timeFraction)}
+            d={calculateClockPointer(timeFraction, finalSize)}
+            strokeWidth={finalSize / 20}
           />
           <circle
-            cx={centerX}
-            cy={centerY}
+            cx={centerX(finalSize)}
+            cy={centerY(finalSize)}
             fill="white"
-            r={radius * 0.5}
+            r={radius(finalSize) * 0.5}
             stroke="black"
           />
           <text
@@ -68,20 +89,15 @@ export const Clock = ({ cycle }: { cycle: StockTimesCycle }) => {
               [styles.lastDay ?? ""]: cycle.endDay - 1 === day
             })}
             dy=".3em"
+            fontSize={finalSize / 5}
             textAnchor="middle"
-            x={centerX}
-            y={centerY}
+            x={centerX(finalSize)}
+            y={centerY(finalSize)}
           >
             {day}
           </text>
         </svg>
       </Flex>
-      <Flex
-        className={clsx(styles.background, {
-          [styles.day ?? ""]: calculatedCycleTime.currentCycle === "day",
-          [styles.night ?? ""]: calculatedCycleTime.currentCycle === "night"
-        })}
-      />
     </>
   );
 };
