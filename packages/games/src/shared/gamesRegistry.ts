2656fe3499efb10dcdb90d43d65f623ebc005a61
Kadhir <kadhirvelm@gmail.com>
2025-02-12 22:04:19 -0800

    Add development tag (#157)

--- packages/games/src/shared/gamesRegistry.ts
+++ packages/games/src/shared/gamesRegistry.ts
@@ -8,8 +8,19 @@ export const GAME_SLUGS = [
   "the-stock-times"
 ] as const;
 
+export interface GameTags {
+  /**
+   * If true, the "in development" tag will not be displayed.
+   */
+  completed?: boolean;
+}
+
 export interface RegisteredGame {
   description: string;
+  /**
+   * Tags for the game.
+   */
+  gameTags: GameTags;
   name: string;
   version: string;
 }
@@ -17,18 +28,23 @@ export interface RegisteredGame {
 export const GAME_REGISTRY: GameRegistry = {
   pong: {
     description: "Classic pong, with a multiplayer twist.",
+    gameTags: {},
     name: "Pong",
     version: "1.0.0"
   },
   "snatch-the-snack": {
     description:
       "A game of speed and strategy to see which team can capture the snacks first.",
+    gameTags: {},
     name: "Snatch the Snack",
     version: "1.0.0"
   },
   "the-stock-times": {
     description:
       "A stock market simulation game. Which team can make the most money?",
+    gameTags: {
+      completed: true
+    },
     name: "The Stock Times",
     version: "1.0.0"
   }
