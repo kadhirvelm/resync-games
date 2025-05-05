e7e129ccdcbbbe799b4f6a0cd808cb6e3462d50b
Kadhir Manickam <kadhirvelm@gmail.com>
2025-05-04 20:07:44 -0700

    Add first art post

---
+++ packages/frontend/src/app/blog/art/background/page.tsx
@@ -0,0 +1,23 @@
+import { BlogEntry } from "@/components/blog/BlogEntry";
+import { BlogText } from "@/lib/resync-components/BlogText";
+
+export default function Background() {
+  return (
+    <BlogEntry>
+      <BlogText>
+        I ran an art group during COVID; many of these pieces came out of that
+        group. We set a theme at the beginning of the month, and then each
+        person interpreted that theme however they wanted. The only catch was
+        that we didn't allow art mediums to repeat, which meant each new person
+        who joined had an increasingly niche medium. My medium was digital art,
+        where I specifically created a series of GIFs and told a story using
+        them.
+      </BlogText>
+      <BlogText>
+        My pieces generally revolve around absurdity. I find it endlessly
+        amusing to keep my audience guessing what wild and weird thing will
+        happen next.
+      </BlogText>
+    </BlogEntry>
+  );
+}
