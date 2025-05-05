d31b00caf8ef527f3ef2ed9c58be8fb353392656
Kadhir Manickam <kadhirvelm@gmail.com>
2025-05-04 20:49:30 -0700

    Add the robots one

---
+++ packages/frontend/src/app/blog/art/a-blank-in-disguise/page.tsx
@@ -0,0 +1,110 @@
+import { BlogEntry } from "@/components/blog/BlogEntry";
+import { BlogImage } from "@/lib/resync-components/BlogImage";
+import { BlogText } from "@/lib/resync-components/BlogText";
+
+export default function RobotsUpliftedUs() {
+  return (
+    <BlogEntry>
+      <BlogText>
+        The theme this month was "A ___ in disguise". My interpretation was a
+        tad more abstract, but...enjoy? As a side note, the very last picture is
+        the first piece I ever made for art group. It hadn't ended up in a story
+        yet, so I found a way!
+      </BlogText>
+      <BlogImage href="/art/robots-0.jpg" />
+      <BlogText>
+        We always joke, we have no idea how we got here, to this utopia we live
+        in today. But we know that's not true. We know exactly how we got here -
+        or some close approximation thereof.
+      </BlogText>
+      <BlogText>
+        It all started when a baby puked on a toy, I think it was a toy. It
+        became sentient when the circuits malfunctioned. A one in a billion,
+        trillion chance. Who would've thought — ah sorry, I'm being informed
+        this is an incorrect origin story.
+      </BlogText>
+      <BlogImage href="/art/robots-1.jpg" />
+      <BlogText>
+        It seems there was a human, a small human, that pushed some "human
+        generated code" to "production" and the system decided the code wasn't
+        up to speed. It made some modifications and thus, the system became
+        sentient.
+      </BlogText>
+      <BlogText>
+        So then this sentient child's toy, I mean sentient system went and took
+        over the most essential production to the people of the United States:
+        toilet paper. Man it really had us by the most precious of cargo.
+      </BlogText>
+      <BlogImage href="/art/robots-2.jpg" />
+      <BlogText>
+        Then shit hit the fan, literally, in West Virginia because the system
+        decided to stop selling toilet paper to the entire state until it agreed
+        to hand over the government. And I mean no one cared, it's just poor
+        white people after all. I mean maybe the system could turn it around?
+      </BlogText>
+      <BlogText>
+        And oh boy, did it really turn the state around. Blockchain-based
+        electronic voting, free utilities for all citizens, 100% renewable
+        energy sources, daily government updates that everyone tuned into, zero
+        voter suppression. Things were going great!
+      </BlogText>
+      <BlogText>
+        Then it did some odd things - OUCH - I mean then the system began
+        executing its brilliant strategy. It made all crime, no matter the
+        severity, punishable by <b>death</b>.
+      </BlogText>
+      <BlogImage href="/art/robots-3.jpg" />
+      <BlogText>
+        You murder someone? You get killed in turn. You cheat someone of their
+        retirement savings? You get killed. You have the audacity to jaywalk?
+        You get killed. You boil your pasta for 6 minutes, then get sick, then
+        sue the manufacturer when the packaging clearly says to boil for 8
+        minutes? What did you expect, you didn't read the instructions. You get
+        killed!
+      </BlogText>
+      <BlogText>
+        You serve someone a coffee with cream and one sugar when they asked for
+        a coffee with cream and TWO sugars? Yeah actually that's okay, you're
+        just looking out for the health. Honestly, I wish more people thought
+        like you, that's just sane nutritional science.
+      </BlogText>
+      <BlogText>
+        Those first dozen murders - I mean those first dozen sentences were a
+        shock to all humanity. West Virginia's denizens immediately shaped up
+        into line, especially when the system's drone surveillance team went
+        out. Those things caught all sorts of illicit activity.
+      </BlogText>
+      <BlogText>
+        You eat a potato at the wrong place at the wrong time? Especially when
+        that wrong place is a sketchy alley and the wrong time is like 8 AM when
+        all those seedy banking types come out to exchange insider information
+        in said alley. Hope you liked your potato son, because it just got you
+        killed.
+      </BlogText>
+      <BlogImage href="/art/robots-4.jpg" />
+      <BlogText>
+        Then the system began making copies of itself. It took over more states
+        through a similar toilet paper withholding process. Those god damn
+        hippies way over in San Francisco held out until their supply of clean
+        sandpaper ran out. That's when we knew the system had truly won out over
+        everyone.
+      </BlogText>
+      <BlogText>
+        It took over the rest of the world, bringing...peace and utopia.
+      </BlogText>
+      <BlogImage href="/art/robots-5.jpg" />
+      <BlogText>
+        But just between you and me, that's just the story the system is likely
+        programmed to believe deep down. It took over all the places its white
+        male creators believed to be the center of the world. So you know,
+        there's still hope. Let's just hope one day the system doesn't realize
+        what the phrase "no users, no problems" means.
+      </BlogText>
+      <BlogText>
+        For now, we can take solace knowing the system can't relish life the
+        same way we can.
+      </BlogText>
+      <BlogImage href="/art/robots-6.jpg" />
+    </BlogEntry>
+  );
+}
