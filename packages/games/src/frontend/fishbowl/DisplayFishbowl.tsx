6c3051d1eac191b9ec5ed033a9411531bc1c8855
Kadhir <kadhirvelm@gmail.com>
2025-06-07 21:57:06 -0700

    Flush out UI for final score (#223)

--- packages/games/src/frontend/fishbowl/DisplayFishbowl.tsx
+++ packages/games/src/frontend/fishbowl/DisplayFishbowl.tsx
@@ -1,10 +1,10 @@
 import { Flex } from "@/lib/radix";
-import { useFishbowlSelector } from "./store/fishbowlRedux";
-import { ContributeWords } from "./playerComponents/components/ContributeWords";
-import { currentPhaseSelector } from "./store/sharedSelectors";
 import { ActivePlayer } from "./playerComponents/components/activePlayer/ActivePlayer";
+import { ContributeWords } from "./playerComponents/components/ContributeWords";
 import { GuessingPlayer } from "./playerComponents/components/guessingPlayer/GuessingPlayer";
 import { PlayerFinalScore } from "./playerComponents/components/PlayerFinalScore";
+import { useFishbowlSelector } from "./store/fishbowlRedux";
+import { currentPhaseSelector } from "./store/sharedSelectors";
 
 export const DisplayFishbowl = () => {
   const phase = useFishbowlSelector(currentPhaseSelector);
