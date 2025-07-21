0b9dd7cb6fc380aa79044db031dcf1e486116de1
Rohan Bavishi <rohan.bavishi95@gmail.com>
2025-07-21 10:55:36 -0400

    Initialize with a fibbage round

--- packages/games/src/backend/trivia/trivia.ts
+++ packages/games/src/backend/trivia/trivia.ts
@@ -1,4 +1,5 @@
-import { IGameServer } from "../base";
+import { CurrentGameState, PlayerId, WithTimestamp } from "@/imports/api";
+import { ICanChangeToState, IGameServer } from "../base";
 
 export interface TriviaGameConfiguration {
   /**
@@ -7,9 +8,92 @@ export interface TriviaGameConfiguration {
   totalRounds: number;
 }
 
-export interface TriviaGame {
-  score: 0;
+export interface TriviaGameScores extends WithTimestamp {
+  /**
+   * The scores of all players in the game.
+   */
+  [playerId: PlayerId]: number;
 }
+
+export interface PlayerGuesses extends WithTimestamp {
+  /**
+   * The guesses made by players in a round.
+   */
+  [playerId: PlayerId]: string;
+}
+
+export interface PlayerAnswers extends WithTimestamp {
+  /**
+   * The submitted by players in a round.
+   */
+  [playerId: PlayerId]: string;
+}
+
+export interface FibbageRound extends WithTimestamp {
+  /**
+   * The answers submitted by players.
+   * Each key is a player ID and the value is the answer they submitted.
+   * The answers are not guaranteed to be unique.
+   */
+  answers: PlayerAnswers;
+  /**
+   * The correct answer for the round.
+   */
+  correctAnswer: string;
+  /**
+   * The guesses made by players.
+   */
+  guesses: PlayerGuesses;
+  /**
+   * The question for the round.
+   */
+  question: string;
+  /**
+   * The round number.
+   */
+  roundNumber: number;
+}
+
+export interface TriviaGame extends WithTimestamp {
+  rounds: FibbageRound[];
+  scores: TriviaGameScores;
+}
+
+const FIBBAGE_QUESTIONS: { answer: string; question: string }[] = [
+  {
+    answer: "Garden Hermit",
+    question:
+      "In the 19th century, wealthy estate owners would sometimes hire a person to live in a small shack in their garden and not wash for years. This person was known as a ______."
+  },
+  {
+    answer: "Backrub",
+    question:
+      "The original name for the search engine that would eventually become Google was ______."
+  },
+  {
+    answer: "Arachibutyrophobia",
+    question:
+      "The fear of having peanut butter stick to the roof of your mouth is called ______."
+  },
+  {
+    answer: "Guinea Pig",
+    question:
+      "In Switzerland, it is illegal to own just one ______ because they are prone to loneliness."
+  },
+  {
+    answer: "Milk",
+    question: "The official state beverage of Kentucky is ______."
+  },
+  {
+    answer: "Grumble",
+    question: "A group of pugs is called a ______."
+  },
+  {
+    answer: "Lego",
+    question:
+      "By number of tires produced, the world's largest tire manufacturer is ______."
+  }
+];
 
 export class TriviaServer
   implements IGameServer<TriviaGame, TriviaGameConfiguration>
@@ -20,9 +104,69 @@ export class TriviaServer
   }> {
     return {
       gameState: {
-        score: 0
+        lastUpdatedAt: new Date().toISOString(),
+        rounds: [],
+        scores: {
+          lastUpdatedAt: new Date().toISOString()
+        }
       },
       version: "1.0.0"
+    };
+  }
+
+  public onChangeState(
+    game: ICanChangeToState<TriviaGame, TriviaGameConfiguration>,
+    newCurrentGameState: CurrentGameState
+  ): TriviaGame | undefined {
+    if (newCurrentGameState !== "playing") {
+      return;
+    }
+
+    // Initialize with a fibbage round.
+    const round = this.initializeFibbageRound(0, game.gameState.rounds || []);
+
+    return {
+      ...game.gameState,
+      rounds: game.gameState.rounds
+        ? [...game.gameState.rounds, round]
+        : [round]
+    };
+  }
+
+  /**
+   * A Fibbage round consists of a random weird question that players are not really supposed to know the answer to.
+   * Players will submit answers that they think are plausible, and then other players will guess which answer is the
+   * correct one.
+   * Players get points for guessing the correct answer and for fooling other players with their answers.
+   * @returns A new Fibbage round.
+   */
+  private initializeFibbageRound(
+    roundNumber: number,
+    existingRounds: FibbageRound[]
+  ): FibbageRound {
+    const existingQuestions = new Set(
+      existingRounds.map((round) => round.question)
+    );
+    const availableQuestions = FIBBAGE_QUESTIONS.filter(
+      (q) => !existingQuestions.has(q.question)
+    );
+
+    const selectedQuestion =
+      availableQuestions[
+        Math.floor(Math.random() * availableQuestions.length)
+      ] || FIBBAGE_QUESTIONS[0];
+
+    if (!selectedQuestion) {
+      throw new Error("No available questions for the Fibbage round.");
+    }
+
+    return {
+      answers: { lastUpdatedAt: new Date().toISOString() },
+      correctAnswer: selectedQuestion.answer,
+      guesses: { lastUpdatedAt: new Date().toISOString() },
+      lastUpdatedAt: new Date().toISOString(),
+      question: selectedQuestion.question,
+      roundNumber
     };
   }
 }
